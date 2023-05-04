26cd3f83ca4fedeb052dc19e5584fe55b7ee0a59
Sebastián Aguirre Vallejo <sebasavmt@gmail.com>
2023-05-04 00:03:56 -0500

    controler  lunes 03 mayo

--- php/controler.php
+++ php/controler.php
@@ -1,5 +1,11 @@
-<?php
+<!doctype html>
+<html lang="es">
+    <head>
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+    </head>
 
+<body>
+<?php
 
 //-----------ENVIAR FORMULARIO DE CONTACTO-----------//
 
@@ -10,7 +16,10 @@ if (isset($_POST['btn_contacto'])) {
     $mensajeContacto = $_POST['mensajeContacto'];
     
     if ($emailContacto == "" || $asuntoContacto == "" || $mensajeContacto == "") {
-        header("location:../shared/alerts/errorEmpty.php");
+        $alerta = ' <div class="alert alert-danger mt-3" role="alert">
+                            Error... Faltan campos por llenar.
+                        </div>';
+        header("Location: ../index.html", true, 303);
     }else {
         $para      = 'asosare@gmail.com';
         $asunto    = $asuntoContacto;
@@ -20,11 +29,41 @@ if (isset($_POST['btn_contacto'])) {
                 'X-Mailer: PHP/' . phpversion();
 
         if (mail($para, $asunto, $mensaje, $de)){
-            echo 'true';
+            
+            $alerta = ' <div class="alert alert-success mt-3" role="alert">
+                            Mensaje Enviado.
+                        </div>';
         }else{
-            echo 'false';
+            
+            $alerta = ' <div class="alert alert-danger mt-3" role="alert">
+                            Error... Mensaje No Enviado.
+                        </div>';
         }
     }
+
+    ?>
+        <div class="modal fade show" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog" style="display: block;">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header justify-content-center">
+                        <h1 class="modal-title fs-5" id="exampleModalCenterTitle">Alerta</h1>
+                    </div>
+                    <div class="modal-body py-5 my-5" >
+                        <?php echo $alerta; ?>
+                    </div>                   
+                    <div class="modal-footer">
+                        <a href="../index.html" class="btn btn-primary">Volver al Inicio</a>
+                    </div> 
+                </div>
+            </div>
+        </div>
+
+    <?php
+}else {
+    header("location:javascript:history.go(-1)");
 }
 
 //-----------FIN ENVIAR FORMULARIO DE CONTACTO-----------//
+?>
+</body>
+</html>
